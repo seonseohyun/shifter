6f2ac8b7566ef48ddfd8f85ca490c29d6c585715
turtle4105 <moonwhale99@gmail.com>
2025-08-10 21:21:22 +0900

    Modified MyInfo UI

--- ViewModels/MyInfoViewModel.cs
+++ ViewModels/MyInfoViewModel.cs
@@ -23,11 +23,64 @@ namespace ShifterUser.ViewModels
             
             _session = session;
             _manager = manager;
-        
+
         }
 
-        
+        // 팝업 표시/숨김
+        [ObservableProperty] private bool isPwPopupVisible;
 
+        // 내정보 기본 속성
+        [ObservableProperty] private string company = "";
+        [ObservableProperty] private string position = "";
+        [ObservableProperty] private string name = "";
+        [ObservableProperty] private string phoneNumber = "";
+        [ObservableProperty] private string id = "";
+
+        // 팝업창 속성
+        [ObservableProperty] private string currentPw = "";
+        [ObservableProperty] private string newPw = "";
+        [ObservableProperty] private string confirmPw = "";
+
+        public bool CanSavePassword =>
+        !string.IsNullOrWhiteSpace(CurrentPw) &&
+        !string.IsNullOrWhiteSpace(NewPw) &&
+        NewPw == ConfirmPw &&
+        NewPw.Length >= 8;
+
+        partial void OnCurrentPwChanged(string value)
+        {
+            OnPropertyChanged(nameof(CanSavePassword));
+            SavePasswordCommand?.NotifyCanExecuteChanged();
+        }
+        partial void OnNewPwChanged(string value)
+        {
+            OnPropertyChanged(nameof(CanSavePassword));
+            SavePasswordCommand?.NotifyCanExecuteChanged();
+        }
+        partial void OnConfirmPwChanged(string value)
+        {
+            OnPropertyChanged(nameof(CanSavePassword));
+            SavePasswordCommand?.NotifyCanExecuteChanged();
+        }
+
+        [RelayCommand]
+        public void OpenChangePassword()
+        {
+            IsPwPopupVisible = true;
+        }
+
+        [RelayCommand]
+        public void ClosePwPopup()
+        {
+            IsPwPopupVisible = false;
+        }
+
+        [RelayCommand(CanExecute = nameof(CanSavePassword))]
+        private async Task SavePasswordAsync()
+        {
+            // TODO: 서버 호출 modify_user_info (current_pw + pw)
+            // 성공 시 팝업 닫기 & 필드 초기화
+        }
 
         [RelayCommand]
         public static void GoBack()
